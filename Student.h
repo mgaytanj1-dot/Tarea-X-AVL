1a33f3af93ce82a40b9a93cc5b6f8ceb20ef3f65
mgaytanj1-dot <mgaytanj1@miumg.edu.gt>
2026-05-10 17:02:59 -0600

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,16 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+using namespace std;
+
+struct Student {
+    int student_id;
+    string full_name;
+    string career;
+    int semester;
+    float gpa;
+    int skill_score;
+};
+
+#endif
